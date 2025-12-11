47a9521c43ddd54dfa324eecbbc7f1b81fed9284
A. Kevin Bailey <akevinbailey@users.noreply.github.com>
2025-12-11 10:34:14 -0600

    Update MuleOnboardTime.py

--- MuleOnboardTime.py
+++ MuleOnboardTime.py
@@ -26,14 +26,14 @@ SOFTWARE.
 Calculate API onboarding time in MuleSoft Anypoint Platform.
 
 User story:
-As a project manager, I want to know the time between an API specification is created
-in Anypoint exchange and when the MuleSoft API specification implementation goes into
-production, so that I can calculate the time it takes to onboard an API.  I also want
-to know the maximum and average times.
+As a project manager, I want to know the time between when an API specification is
+loaded into Anypoint Exchange and when the MuleSoft API specification implementation
+goes into production in API Manager, so that I can calculate the time it takes to
+onboard an API. I also want to know the maximum and average times.
 - Since this is an API specification the definition of production is when the API is
   published in the API Manager production environment (PROD)
 - For large numbers of APIs, this report will need to use page sizes to limit the
-  number of APIs returned on each call and multiple calls for each page is required.
+  number of APIs returned on each call and requires additional calls for each page.
 
 This version links Exchange API definition name to API Manager instances name:
 - Exchange asset "assetId" (or name if assetId missing)
